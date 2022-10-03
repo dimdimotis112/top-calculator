bcefe8af9440c1e9bac612b0b9d177e03cdcd935
dimdimotis112 <normixslv@gmail.com>
2022-10-03 17:15:42 +0300

    redo calculate(), create function operatorClick()

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 const screen = document.querySelector('#screen');
-
 const buttons = document.querySelectorAll('button');
+let value1 = 0; // creates value1 as 0, so if user presses Enter immediately, it just outputs zero.
+let operatorClicked = false;
+let operatorUsed = '';
 
 for (let button of buttons) {
     button.addEventListener('click', buttonPress);
 }
 
 function buttonPress() {
-    if (this.textContent === 'Enter') {
+    if (operatorClicked) {
+        screen.textContent = '';
+    }
+    if (this.textContent === 'Enter' ) {
         calculate();
         return;
+    } else if (this.classList.contains('operator')) {
+        operatorClick(this);
+        return;
     }
+    operatorClicked = false;
     screen.textContent += this.textContent;
 }
 
-function calculate() {
-    let operation = screen.textContent;
-    operation = operation.split(/([+-/XS])/);
+function operatorClick(operatorButton) {
+    operatorUsed = operatorButton.textContent;
+    value1 = Number(screen.textContent);
+    operatorClicked = true;
+}
 
-    const result = operate(operation[1], +operation[0], +operation[2]);
+function calculate() {
+    let value2 = Number(screen.textContent);
+
+    const result = operate(operatorUsed, value1, value2);
+
     screen.textContent = result;
 }
 
